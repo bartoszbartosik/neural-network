1b0f8563493c47c3a5ad0d9a1f307d31fcf6a875
Bartosz Bartosik <babosz189@gmail.com>
2024-03-16 19:58:30 +0100

    Simplified derivative function

--- neuralnetwork/activations/__init__.py
+++ neuralnetwork/activations/__init__.py
@@ -3,22 +3,22 @@ from typing import Callable
 import numpy as np
 
 
+# Return derivative value of a given function in a given point
+def d(f: Callable, x: np.ndarray):
+    match f.__name__:
+        case 'sigmoid': return f(x)*(1 - f(x))
+        case 'relu': return 1 * (x > 0)
+        case 'linear': return 1
+
+
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 
 # Rectified Linear Unit
 def relu(x):
-    return 0 if x <= 0 else x
+    return x * (x > 0)
 
 
 def linear(x):
     return x
-
-
-# Return derivative value of a given function in a given point
-def derivative(function: Callable, x: np.ndarray):
-    match function.__name__:
-        case 'sigmoid': return function(x)*(1-function(x))
-        case 'relu': return 0 if x <= 0 else 1
-        case 'linear': return 1
