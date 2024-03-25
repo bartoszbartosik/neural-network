99f8cf48f77b9b148bc0fec5a8b86521a02aecc9
Bartosz Bartosik <babosz189@gmail.com>
2024-03-25 23:22:55 +0100

    Added cross entropy loss function

--- neuralnetwork/losses/__init__.py
+++ neuralnetwork/losses/__init__.py
@@ -6,6 +6,7 @@ import numpy as np
 def d(function: Callable, prediction: np.ndarray, target: np.ndarray):
     match function.__name__:
         case 'mse': return 2*(prediction - target)
+        case 'cross_entropy': return (1 - target) / (1 - prediction) - target / prediction
 
 
 def mse(prediction, target):
@@ -13,3 +14,7 @@ def mse(prediction, target):
     Return total Mean Squared Error
     """
     return np.mean((target - prediction)**2)
+
+
+def cross_entropy(prediction, target):
+    return -np.mean(target * np.log(prediction) + (1 - target) * np.log(1-prediction))
