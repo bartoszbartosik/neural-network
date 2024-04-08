Fixed conv input gradient computation

neuralnetwork/layers/conv.py
@@ -108,19 +108,24 @@ class Convolutional(Layer):
 
     def backpropagate(self, grad: np.ndarray, a_: Layer, w_out: Layer = None) -> tuple:
 
-        batch_size, out_rows, out_cols, out_channels = a_.shape
+        batch_size, _, _, _ = self.shape
+        if w_out is not None:
+            out_channels = w_out.shape[0]
+        in_channels = self.w.shape[1]
 
         grad_b = np.zeros(self.shape)
         grad_w = np.zeros_like(self.w)
 
         for b in range(batch_size):
             for k in range(self.knum):
-                for c in range(out_channels):
-                    grad_pad = pad_like(grad[b, :, :, c], grad_b[b, :, :, c])
-                    # grad_b[b, :, :, c] += convolve(grad_pad, self.w[k, c, :, :], 'same')
-                    grad_b[b, :, :, c] += grad_pad if w_out is None else convolve(grad_pad, w_out[b, c, :, :], 'same')
-                    grad_b[b, :, :, c] *= activations.d(self.activation, self.z[b, :, :, k])
-                    grad_w[b, k, :, :] += crosscorrelate(a_[b, :, :, c], grad[b, :, :, c], 'valid')
+                if w_out is None:
+                    grad_b[b, :, :, k] = grad[b, :, :, k]
+                else:
+                    for c_ in range(out_channels):
+                        grad_b[b, :, :, k] += convolve(grad[b, :, :, c_], w_out[c_, k, :, :], 'same')
+
+                for c in range(in_channels):
+                    grad_w[k, c, :, :] += crosscorrelate(a_[b, :, :, c], grad[b, :, :, k], 'valid')
 
         return grad_b, grad_w
 

neuralnetwork/layers/maxpool.py
@@ -13,7 +13,7 @@ class MaxPooling(Layer):
         self.pool_size = pool_size
         self.stride = stride
         self.amax = np.zeros([])
-        self.indices = []
+        self.indices = np.empty([], dtype=object)
 
 
 
@@ -34,8 +34,7 @@ class MaxPooling(Layer):
         batch_size, out_rows, out_cols, channels = self.shape
         p_rows, p_cols = self.pool_size
 
-        self.indices = []
-
+        self.indices = np.empty(self.shape, dtype=object)
         for b in range(batch_size):
             for c in range(channels):
                 for row in range(out_rows):
@@ -47,13 +46,14 @@ class MaxPooling(Layer):
                         # Update MaxPool activations
                         self.a[b, row, col, c] = window_max
 
-                        # Get the max value's index
+                        # Get the max value's 2D index
                         r_max, c_max = np.unravel_index(np.argmax(window), window.shape)
                         r_max += row * self.stride
                         c_max += col * self.stride
 
                         # Store indices
-                        self.indices.append((r_max, c_max))
+                        self.indices[b, row, col, c] = r_max, c_max
+                        # self.indices.append((r_max, c_max))
 
 
     def backpropagate(self, grad: np.ndarray, a_: Layer, w_out: Layer = None) -> tuple:
@@ -63,11 +63,13 @@ class MaxPooling(Layer):
         batch_size, rows, cols, channels = self.shape
 
         grad_b = np.zeros_like(a_)
+        grad = grad.reshape(self.shape)
 
         for b in range(batch_size):
             for c in range(channels):
-                for i, index in enumerate(self.indices):
-                    grad_b[b, :, :, c][index] += grad[b][i]
+                for row in range(len(self.indices[b, :, :, c])):
+                    for col in range(len(self.indices[b, row, :, c][0])):
+                        grad_b[b, :, :, c][self.indices[b, row, col, c]] = grad[b, row, col, c]
 
         return grad_b, None
 

test/test_conv.py
@@ -136,10 +136,9 @@ class TestConv(unittest.TestCase):
         net = Network()
         net.add_layer(InputLayer(input_shape=(batch_size, *input_shape)))
         net.add_layer(Convolutional(1, kernel_size, activation=relu, padding='same'))
-        net.add_layer(Convolutional(1, kernel_size, activation=relu, padding='valid'))
-        net.add_layer(MaxPooling((2, 2), 1))
+        net.add_layer(Convolutional(1, kernel_size, activation=relu, padding='same'))
+        net.add_layer(MaxPooling((2, 2), 2))
         net.add_layer(Flatten())
-        net.add_layer(Dense(4, activation=sigmoid))
         net.compile(losses.mse)
 
         net.layers[1].w = kernel_1.reshape(net.layers[1].w.shape)
@@ -153,7 +152,46 @@ class TestConv(unittest.TestCase):
         print(a)
 
         # Expected output
-        y = np.array([[0, 1, 0, 1]])
+        y = np.array([[0, 0, 1, 1]])
+
+        # Train model
+        net.fit(x.reshape(batch_size, *input_shape), y, epochs=10, batch_size=1, learning_rate=0.3)
+
+        # Predict trained model output
+        a = net.predict(x.reshape(batch_size, *input_shape))
+        print(a)
+        np.testing.assert_allclose(a, y, rtol=1e-5)
+
+    def test_prediction_4(self):
+        # Define input shape
+        batch_size = 1
+        input_shape = (4, 4, 1)  # (rows, cols, channels)
+
+        # Define sample input
+        x = np.array([
+            [.7, -.3, -.7, .5],
+            [.9, -.5, -.2, .9],
+            [-.1, .8, -.3, -.5],
+            [0, .2, -.1, .6]
+        ])
+
+
+        # Build model
+        net = Network()
+        net.add_layer(InputLayer(input_shape=(batch_size, *input_shape)))
+        net.add_layer(Convolutional(4, (2, 2), activation=relu, padding='same'))
+        net.add_layer(Convolutional(2, (2, 2), activation=relu, padding='same'))
+        net.add_layer(MaxPooling((2, 2), 2))
+        net.add_layer(Flatten())
+        net.compile(losses.mse)
+
+
+        # Predict not trained model output
+        a = net.predict(x.reshape(batch_size, *input_shape))
+        print(a)
+
+        # Expected output
+        y = np.array([[0, 1, 1, 1, 1, 0, 0, 0]])
 
         # Train model
         net.fit(x.reshape(batch_size, *input_shape), y, epochs=20, batch_size=1, learning_rate=0.3)